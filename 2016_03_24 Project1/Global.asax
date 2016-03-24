30add869deb099ebcaee6c44f43bb9065aa15592
1102137238 <1102137238@gm.kuas.edu.tw>
2016-03-24 13:49:00 +0800

    OK

---
+++ 2016_03_24 Project1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2016_03_24_Project1.MvcApplication" Language="C#" %>
---
+++ 2016_03_24 Project1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2016_03_24_Project1.Startup))]
+namespace _2016_03_24_Project1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
